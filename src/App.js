b8f620b6dc39b8cf5ad08d3df3a051c56020303d
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-04-14 09:16:48 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? 'dark' : ''}>
+    <div className={darkTheme && 'dark'}>
       <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
         <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
         <Routes />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Footer = () => (
-  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
-    © 2021 Goggl, Inc.
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+      © {currentYear} Goggl, Inc.
+    </div>
+  );
+};
 
 export default Footer;
